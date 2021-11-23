302f90338ba66739ccdccd906933a0b660f97cab
John David Carroll <johndavidcarroll@gmail.com>
2021-11-23 15:20:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
+function saturdayFun(activity='roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
 }
 
 let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
+  return `This Monday, I will ${activity}.`;
 }
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
+let wrapAdjective = function(style='*'){
+  return function(adjective='special'){
     return `You are ${style}${adjective}${style}!`
   }
 }
+const encouragingPromptFunction = wrapAdjective("!!!");
